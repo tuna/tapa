561d6ff08180bb953b0c26b685553210596e136c
Hank Or <hnkor@proton.me>
2026-07-21 23:22:59 -0700

    fix(dse): serialize Vivado synthesis jobs

--- tapa-core/tapa-xilinx/src/tools/vitis/mod.rs
+++ tapa-core/tapa-xilinx/src/tools/vitis/mod.rs
@@ -238,6 +238,8 @@ fn build_invocation(resolved: &ResolvedJob<'_>) -> ToolInvocation {
         .arg(job.target_mhz.to_string())
         .arg("--connectivity.nk")
         .arg(format!("{}:1:{}", job.kernel_name, job.kernel_name))
+        .arg("--vivado.synth.jobs")
+        .arg("1")
         .arg("--vivado.prop=run.impl_1.STEPS.PHYS_OPT_DESIGN.IS_ENABLED=1")
         .arg(format!(
             "--vivado.prop=run.impl_1.STEPS.OPT_DESIGN.ARGS.DIRECTIVE={IMPLEMENTATION_STRATEGY}"
@@ -486,6 +488,8 @@ ap_clk             -0.100       -1.000
                 "300",
                 "--connectivity.nk",
                 "Top:1:Top",
+                "--vivado.synth.jobs",
+                "1",
                 "--vivado.prop=run.impl_1.STEPS.PHYS_OPT_DESIGN.IS_ENABLED=1",
                 "--vivado.prop=run.impl_1.STEPS.OPT_DESIGN.ARGS.DIRECTIVE=Explore",
                 "--vivado.prop=run.impl_1.STEPS.PLACE_DESIGN.ARGS.DIRECTIVE=EarlyBlockPlacement",
